f25676520befb9ff286f121e638eb229d17d1c8c
Swapnil Sharma <nilinswap@gmail.com>
2021-09-06 13:02:17 +0530

    tcp ready for profiling

--- socket_demo/tcp/client.py
+++ socket_demo/tcp/client.py
@@ -8,7 +8,15 @@ def main():
     while True:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect(('127.0.0.1', 12345))
-        print("received at client", s.recv(1024).decode())
+
+        sent_msg = "message from client {}".format(s.getsockname());
+
+        s.send(sent_msg)
+
+        print("sent: sent to server", sent_msg)
+        rec_msg = s.recv(1024).decode()
+        print("rec: received at client", rec_msg)
+
         time.sleep(5)
         s.close()
 
--- socket_demo/tcp/server.py
+++ socket_demo/tcp/server.py
@@ -1,7 +1,16 @@
+"""
+server.py
+"""
 import socket
 
 
+BUFFER_SIZE = 1024
+
+
 def main():
+    """
+    :return:
+    """
     s = socket.socket()
     print("socket created successfully")
 
@@ -15,12 +24,17 @@ def main():
 
     while True:
         print("here at start")
+
         c, addr = s.accept()
         print("'Got connection from", addr)
+        rec_msg = c.recv(BUFFER_SIZE)
+        print("rec:", rec_msg)
 
-        c.send('Thank you for connecting'.encode())
+        sent_msg = 'Thank you for connecting'.encode()
+        c.send(sent_msg)
+        print("sent:", sent_msg)
 
         c.close()
-
+        print("\n\n")
 
 main()
