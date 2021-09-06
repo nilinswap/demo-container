ed57cc9ab4b24efe4bb7c8588f89af97b39cd84b
Swapnil Sharma <nilinswap@gmail.com>
2021-09-06 11:16:11 +0530

    udp server up and running

--- socket_demo/tcp/client.py
+++ socket_demo/tcp/client.py
@@ -1,11 +1,13 @@
+""" Client"""
 import socket
 import time
 
-def main():
 
+def main():
+    """tcp client main"""
     while True:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        v = s.connect(('127.0.0.1', 12345))
+        s.connect(('127.0.0.1', 12345))
         print("received at client", s.recv(1024).decode())
         time.sleep(5)
         s.close()
